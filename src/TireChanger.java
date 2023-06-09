e0cade30625fa681cd0755d19c2a9f183fe43ad1
Kseniia313 <kshome@mail.ru>
2023-06-09 18:12:58 +0300

    Homework is done

---
+++ src/TireChanger.java
@@ -0,0 +1,11 @@
+public interface TireChanger {
+    public default void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+    public default void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+    public default void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
---
+++ src/Truck.java
@@ -0,0 +1,29 @@
+
+public class Truck implements TireChanger{
+
+    private String modelName;
+    private int wheelsCount;
+
+    public Truck(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+
+}
